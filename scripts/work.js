1c0c1bf60bd91a3b3d68a0c663cda8ceea93ec87
sherry_chypre <kbingjie@foxmail.com>
2022-07-11 17:20:50 -0500

    Add work experience

--- scripts/work.js
+++ scripts/work.js
@@ -1,6 +1,14 @@
 export default [
     {
         id: 1,
+        title: "Software Engineer II",
+        company: "Walmart Global Tech",
+        location: "Bentonville, AR",
+        jobDescription: "Working on different alerting and monitoring scripts to detect potential issues in the business with team members. Reviewing and Refactoring existing Python scripts to improve efficiency performance and reduce redundancy.Built an automated emailing app with Python, Flask framework, HTML5/CSS, and JavaScript to improve work efficiency.Working with external vendors to resolve their technical problems.",
+        date: "10/2021-Current"
+    },
+    {
+        id: 2,
         title: "Operations Coordinator",
         company: "Gravy.Live",
         location: "Madison, WI",
@@ -8,19 +16,19 @@ export default [
         date: "04/2019-01/2020"
     },
     {
-        id: 2,
+        id: 3,
         title: "Game Operation Specialist",
         company: "Happy Alliance",
         location: "CHENGDU, CN",
         jobDescription: "Provided customer service, collaborated with development team to identify complex issues and to find solutions for customers with accuracy. Monitored user data daily to identify unusual game activities, and alerted development team. Analyzed client’s software requirements and wrote functional requirements designs for development and design team. Provided feedback to development team with respect to game software and management tools. Performed project management activates by interacting with managers, marketing, and development team regarding project resources, status, and timeline. Made sure project updating plan and monthly game event plan executed accurately. Validated monthly game revenue data and delivered reports to stakeholders. Created configuration documentation for project monthly events and new functions. Assisted client and development team with software version releases for multiple markets. Performed User Acceptance Testing to verify build before delivery.",
         date: "09/2015 - 08/2018"
     },
-    {
-        id: 3,
-        title: "Business Development Specialist",
-        company: "B-Ray DreamWork",
-        location: "CHENGDU, CN",
-        jobDescription: "Provided configuration documents translation for 2 web-game projects launches and monthly updates in South-east Asian markets. Worked daily with develop teams and clients on function changes requirements. Coordinated various teams to prepare for project applications/documents for client testing and Collected feedback to develop team. Built good relationship with international partners and potential clients.",
-        date: "06/2012 - 12/2013"
-    }
+    // {
+    //     id: 4,
+    //     title: "Business Development Specialist",
+    //     company: "B-Ray DreamWork",
+    //     location: "CHENGDU, CN",
+    //     jobDescription: "Provided configuration documents translation for 2 web-game projects launches and monthly updates in South-east Asian markets. Worked daily with develop teams and clients on function changes requirements. Coordinated various teams to prepare for project applications/documents for client testing and Collected feedback to develop team. Built good relationship with international partners and potential clients.",
+    //     date: "06/2012 - 12/2013"
+    // }
 ]
